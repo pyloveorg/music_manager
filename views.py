0f70951377fb12f532df85a47969d6f6a0de1d8d
Radek Z <zuchol@op.pl>
2018-02-02 20:06:13 +0100

    Dodanie obsługi strony "page not found"

--- views.py
+++ views.py
@@ -11,3 +11,13 @@ from flask import render_template
 @app.route('/', methods=['GET', 'POST'])
 def info():
     return render_template('info.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    return render_template('register.html')
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
